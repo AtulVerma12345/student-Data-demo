164715832f17cef6ed547b98b45f697d003c1813
Atul <atul.raj28@gmail.com>
2018-06-29 14:26:34 +0530

    Removed unUsed code

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Welcome from '../home/welcome';
-import About from '../about/about';
 import DetailViewPage from '../DetailsView/DetailsViewPage';
-import ProductForm from '../FormMaterialUi/Form';
 
 const App = () => (
   <div>
     <main>
       <Route exact path="/" component={Welcome} />
-      <Route exact path="/about-us" component={About} />
       <Route exact path="/DetailViewPage" component={DetailViewPage} />
-      <Route exact path="/ProductForm" component={ProductForm} />
     </main>
   </div>
 );
